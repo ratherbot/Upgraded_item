066816074a845b60c77edcbab447e579e521c4fe
ratherbot <just51290@mail.ru>
2025-12-15 16:12:28 +0500

    pn

--- RH AI/qlora/qloraapp/urls.py
+++ RH AI/qlora/qloraapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('jobs/', views.collect_jobs, name='jobs'),
-    path('vacancies/', views.collect_jobs, name='vacancies'),
-    path('project_data/', views.collect_jobs, name='project_data'),
-    path('statistics/', views.collect_jobs, name='statistics'),
+    path('vacancies/', views.analyze_vacancies, name='vacancies'),
+    path('project_data/', views.analyze_project_data, name='project_data'),
+    path('statistics/', views.matrices_and_statistics, name='statistics'),
 ]
